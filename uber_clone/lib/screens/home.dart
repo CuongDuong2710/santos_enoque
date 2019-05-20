a2d8fd5b1fcf82bbe85b8d903ba6d1ad07d72029
cuong.duong <cuong.duong@titancorpvn.com>
2019-05-20 14:22:09 +0700

    get current user's location

--- uber_clone/lib/screens/home.dart
+++ uber_clone/lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:uber_clone/requests/google_maps_requests.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -24,120 +26,137 @@ class Map extends StatefulWidget {
 
 class _MapState extends State<Map> {
   GoogleMapController mapController;
-  static const _initialPosition = LatLng(12.97, 77.58);
-  LatLng _lastPosition = _initialPosition;
+  GoogleMapsServices googleMapsServices = new GoogleMapsServices();
+  TextEditingController locationController = TextEditingController();
+  TextEditingController destinationController = TextEditingController();
 
+  static LatLng _initialPosition;
+  LatLng _lastPosition = _initialPosition;
   final Set<Marker> _markers = {};
+  final Set<Polyline> _polylines = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        GoogleMap(
-          initialCameraPosition:
-              CameraPosition(target: _initialPosition, zoom: 10.0),
-          onMapCreated: onCreated,
-          myLocationEnabled: true,
-          mapType: MapType.normal,
-          compassEnabled: true,
-          markers: _markers,
-          onCameraMove: _onCameraMove,
-        ),
-        Positioned(
-          top: 50.0,
-          right: 15.0,
-          left: 15.0,
-          child: Container(
-            height: 50.0,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(3.0),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(1.0, 5.0),
-                    blurRadius: 10,
-                    spreadRadius: 3)
-              ],
+    return _initialPosition == null
+        ? Container(
+            alignment: Alignment.center,
+            child: Center(
+              child: CircularProgressIndicator(),
             ),
-            child: TextField(
-              cursorColor: Colors.black,
-              // controller: locationController,
-              decoration: InputDecoration(
-                icon: Container(
-                  margin: EdgeInsets.only(left: 20, top: 5),
-                  width: 10,
-                  height: 10,
-                  child: Icon(
-                    Icons.location_on,
-                    color: Colors.black,
+          )
+        : Stack(
+            children: <Widget>[
+              GoogleMap(
+                initialCameraPosition:
+                    CameraPosition(target: _initialPosition, zoom: 10.0),
+                onMapCreated: onCreated,
+                myLocationEnabled: true,
+                mapType: MapType.normal,
+                compassEnabled: true,
+                markers: _markers,
+                onCameraMove: _onCameraMove,
+              ),
+              Positioned(
+                top: 50.0,
+                right: 15.0,
+                left: 15.0,
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(1.0, 5.0),
+                          blurRadius: 10,
+                          spreadRadius: 3)
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    controller: locationController,
+                    decoration: InputDecoration(
+                      icon: Container(
+                        margin: EdgeInsets.only(left: 20, top: 5),
+                        width: 10,
+                        height: 10,
+                        child: Icon(
+                          Icons.location_on,
+                          color: Colors.black,
+                        ),
+                      ),
+                      hintText: "pick up",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+                    ),
                   ),
                 ),
-                hintText: "pick up",
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
               ),
-            ),
-          ),
-        ),
-        Positioned(
-          top: 105.0,
-          right: 15.0,
-          left: 15.0,
-          child: Container(
-            height: 50.0,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(3.0),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(1.0, 5.0),
-                    blurRadius: 10,
-                    spreadRadius: 3)
-              ],
-            ),
-            child: TextField(
-              cursorColor: Colors.black,
-              // controller: destinationController,
-              textInputAction: TextInputAction.go,
-              onSubmitted: (value) {
-                // sendRequest(value);
-              },
-              decoration: InputDecoration(
-                icon: Container(
-                  margin: EdgeInsets.only(left: 20, top: 5),
-                  width: 10,
-                  height: 10,
-                  child: Icon(
-                    Icons.local_taxi,
-                    color: Colors.black,
+              Positioned(
+                top: 105.0,
+                right: 15.0,
+                left: 15.0,
+                child: Container(
+                  height: 50.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(3.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(1.0, 5.0),
+                          blurRadius: 10,
+                          spreadRadius: 3)
+                    ],
+                  ),
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    // controller: destinationController,
+                    textInputAction: TextInputAction.go,
+                    onSubmitted: (value) {
+                      // sendRequest(value);
+                    },
+                    decoration: InputDecoration(
+                      icon: Container(
+                        margin: EdgeInsets.only(left: 20, top: 5),
+                        width: 10,
+                        height: 10,
+                        child: Icon(
+                          Icons.local_taxi,
+                          color: Colors.black,
+                        ),
+                      ),
+                      hintText: "destination?",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
+                    ),
                   ),
                 ),
-                hintText: "destination?",
-                border: InputBorder.none,
-                contentPadding: EdgeInsets.only(left: 15.0, top: 16.0),
               ),
-            ),
-          ),
-        ),
-        /* Positioned(
-          top: 40,
-          right: 10,
-          child: FloatingActionButton(
-            onPressed: _onAddMarkerPressed,
-            tooltip: 'add marker',
-            backgroundColor: Colors.black,
-            child: Icon(
-              Icons.add_location,
-              color: Colors.white,
-            ),
-          ),
-        ) */
-      ],
-    );
+              /* Positioned(
+              top: 40,
+              right: 10,
+              child: FloatingActionButton(
+                onPressed: _onAddMarkerPressed,
+                tooltip: 'add marker',
+                backgroundColor: Colors.black,
+                child: Icon(
+                  Icons.add_location,
+                  color: Colors.white,
+                ),
+              ),
+            ) */
+            ],
+          );
   }
 
   void onCreated(GoogleMapController controller) {
@@ -163,11 +182,11 @@ class _MapState extends State<Map> {
   }
 
   /*
-* [12.12, 312.2, 321.3, 231.4, 234.5, 2342.6, 2341.7, 1321.4]
-* (0-------1-------2------3------4------5-------6-------7)
-* */
+    * [12.12, 312.2, 321.3, 231.4, 234.5, 2342.6, 2341.7, 1321.4]
+    * (0-------1-------2------3------4------5-------6-------7)
+    * */
 
-//  this method will convert list of doubles into latlng
+  //  this method will convert list of doubles into latlng
   List<LatLng> convertToLatLng(List points) {
     List<LatLng> result = <LatLng>[];
     for (int i = 0; i < points.length; i++) {
@@ -184,7 +203,7 @@ class _MapState extends State<Map> {
     int index = 0;
     int len = poly.length;
     int c = 0;
-// repeating until all attributes are decoded
+    // repeating until all attributes are decoded
     do {
       var shift = 0;
       int result = 0;
@@ -204,11 +223,23 @@ class _MapState extends State<Map> {
       lList.add(result1);
     } while (index < len);
 
-/*adding to previous value as done in encoding */
+    /*adding to previous value as done in encoding */
     for (var i = 2; i < lList.length; i++) lList[i] += lList[i - 2];
 
     print(lList.toString());
 
     return lList;
+  }
+
+  void _getUserLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    List<Placemark> placemark = await Geolocator()
+        .placemarkFromCoordinates(position.latitude, position.longitude);
+    print("placemark $placemark");
+    setState(() {
+      _initialPosition = LatLng(position.latitude, position.longitude);
+      locationController.text = placemark[0].name;
+    });
   }
 }
